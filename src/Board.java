27176334d7d05e218951299a793c58bf8ef33d91
Fozzzzy <122066488+andrewtedja@users.noreply.github.com>
2025-02-19 01:58:51 +0700

    feat: added Board functions for placing pieces

--- src/Board.java
+++ src/Board.java
@@ -14,6 +14,7 @@ public class Board {
     private int rows;
     private int cols;
 
+    // ? Constructor
     public Board(FileData fileData) {
         this.rows = fileData.getN();
         this.cols = fileData.getM();
@@ -25,6 +26,12 @@ public class Board {
             }
         }
     }
+
+
+    // ? Getters and setters
+    public int getRows() { return rows; }
+    public int getCols() { return cols;}
+
     public void printBoard() {
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
@@ -34,20 +41,53 @@ public class Board {
         }
     }
 
-    public boolean canPlacePiece(Piece piece, int startRow) {
-        for (int i = 0; i < piece.getShape().length; i++) {
-            for (int j = 0; j < piece.getShape()[0].length; j++) {
-                if (piece.getShape()[i][j] != '.' && grid[startRow + i][j] != '.') {
+    public boolean canPlacePiece(Piece piece, int startRow, int startCol) {
+        char[][] shape = piece.getShape();
+        int pieceRows = shape.length;
+        int pieceCols = shape[0].length;
+
+        // Check if piece fits
+        if (startRow < 0 || startCol < 0|| startRow + pieceRows > rows || startCol + pieceCols > cols) {
+            return false;
+        }
+
+        // Check if piece overlaps or not
+        for (int i = 0; i < pieceRows; i++) {
+            for (int j = 0; j < pieceCols; j++) {
+                if (shape[i][j] != '.' && grid[startRow + i][startCol + j] != '.') {
                     return false;
                 }
             }
         }
+
         return true;
+    }           
+
+    public boolean placePiece(Piece piece, int startRow, int startCol) {
+        if (!canPlacePiece(piece, startRow, startCol)) {
+            return false;
+        }
+        char[][] shape = piece.getShape();
+        for (int i = 0; i < shape.length; i++) {
+            for (int j = 0; j < shape[0].length; j++) {
+                if (shape[i][j] != '.') {
+                    grid[startRow + i][startCol + j] = shape[i][j];
+                }
+            }
+        }
+        return true;
+    }   
+
+    public void removePiece(Piece piece, int startRow, int startCol) {
+        char[][] shape = piece.getShape();
+        for (int i = 0; i < shape.length; i++) {
+            for (int j = 0; j < shape[0].length; j++) {
+                if (shape[i][j] != '.') {
+                    grid[startRow + i][startCol + j] = '.';
+                }
+            }
+        }
     }
-
-
-    // todo -> Add Pieces
-    // todo -> Remove Pieces
 }
 
 
